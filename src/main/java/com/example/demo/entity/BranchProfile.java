68a3cc7db3723115ea38f2bfa7b50d44981b722a
bala-amypo <bala@amypo.in>
2025-12-17 06:15:56 +0000

    User commit

--- src/main/java/com/example/demo/entity/BranchProfile.java
+++ src/main/java/com/example/demo/entity/BranchProfile.java
@@ -1,7 +1,7 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Id;
-import jakarta.persistence.lastSync
+import jakarta.persistence.lastSyncAt;
 
 @Entity
 public class BranchProfile {
