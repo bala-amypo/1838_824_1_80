de7cbd8d6567a5dde95dd6443fc8a5ec4a35791d
bala-amypo <bala@amypo.in>
2025-12-17 06:45:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/AcademicEvent.java
+++ src/main/java/com/example/demo/entity/AcademicEvent.java
@@ -3,5 +3,13 @@
 
 
 public class AcademicEvent {
-    private Long 
+    private Long id;
+    private Long branchId;
+    private String title;
+    private String eventType;
+    private String startDate;
+    private String endDate;
+    private String location;
+    private String description;
+    private String submittedAt;
 }
