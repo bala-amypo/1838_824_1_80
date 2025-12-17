da380c2e9f78c8c9f6cf36670df4b19db303d0a4
bala-amypo <bala@amypo.in>
2025-12-17 08:48:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/AcademicEvent.java
+++ src/main/java/com/example/demo/entity/AcademicEvent.java
@@ -1,7 +1,9 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
 
 
-
-
+@Entity
 public class AcademicEvent {
     private Long id;
     private Long branchId;
@@ -12,4 +14,12 @@ public class AcademicEvent {
     private String location;
     private String description;
     private String submittedAt;
+    public AcademicEvent() {
+
+    }
+    public void ValidateAndInitializer() {
+        this.submittedAt = LocalDateTime.now();
+        if (startDate != null && endDate != null && startDate.isAfter(endDate)) {
+            throw new RuntimeException("ValidationException: startDate must be before or equal to endDate.")
+    }
 }
