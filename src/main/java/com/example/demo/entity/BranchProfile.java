a669f092b96ee822dc4ba3e290a01eafa0ff3c36
bala-amypo <bala@amypo.in>
2025-12-17 05:33:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/BranchProfile.java
+++ src/main/java/com/example/demo/entity/BranchProfile.java
@@ -4,5 +4,8 @@ package com.example.demo.entity;
 @Entity
 public class BranchProfile {
     private Long id;
-    private Long 
+    private String branchcode;
+    private String title;
+    private String eventType;
+    private String LocalDate
 }
