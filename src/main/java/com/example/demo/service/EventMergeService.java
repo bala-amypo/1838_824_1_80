a5791a5ebf6a4b393d0308e0165cd26f63fdfd55
bala-amypo <bala@amypo.in>
2025-12-18 13:30:47 +0000

    User commit

--- src/main/java/com/example/demo/service/EventMergeService.java
+++ src/main/java/com/example/demo/service/EventMergeService.java
@@ -5,5 +5,8 @@ import com.example.demo.entity.EventMergeRecord;
 
 @Service
 public interface EventMergeService {
-    public EventMerge 
+    public EventMerge mergeEvents(List<Long> eventIds, string reason);
+    public EventMerge getAllMergeRrecord();
+    public EventMerge getMergeRecordById(Long id);
+    public EventMerge getMergeRecordByDate(LocalDate start, LocalDate end)
 }
