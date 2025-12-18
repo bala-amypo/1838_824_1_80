1d5e162816d3ccec9859f9fe62b572043db334ef
bala-amypo <bala@amypo.in>
2025-12-18 05:11:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/EventMergeRecord.java
+++ src/main/java/com/example/demo/entity/EventMergeRecord.java
@@ -8,5 +8,11 @@ public class EventMergeRecord {
     private String mergedTitle;
     private LocalDate mergedStartDate;
     private LocalDate mergedEndDate;
-    private
+    private String mergeReason;
+    private LocalDateTime createdAt;
+    public void ValidateAndInitializer() {
+        this.createdAt = LocalDateTime.now();
+        this.mergedStartDate = LocalDate.now();
+        this mergedEndDate = LocalDate.now();
+    }
 }
