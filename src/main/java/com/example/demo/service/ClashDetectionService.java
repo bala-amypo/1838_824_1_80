bc9fc45c6017315ccfdce793c893904eae9cfd1c
bala-amypo <bala@amypo.in>
2025-12-18 13:21:47 +0000

    User commit

--- src/main/java/com/example/demo/service/ClashDetectionService.java
+++ src/main/java/com/example/demo/service/ClashDetectionService.java
@@ -1,4 +1,10 @@
 package com.example.demo.service;
 
 import jakarta.persistence.service;
-import com.example.demo.
+import com.example.demo.entity.ClashRecord;
+
+public interface ClashDetectionService {
+    public ClashRecord logClash(ClashRecord clash);
+    public ClashgetClashesForEvent(Long eventId);
+
+}
