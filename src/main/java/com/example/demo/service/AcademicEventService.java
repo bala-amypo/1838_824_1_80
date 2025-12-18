8ae3df16ae8f10f0a0da73f2b9f35a639e462fa7
bala-amypo <bala@amypo.in>
2025-12-18 13:06:47 +0000

    User commit

--- src/main/java/com/example/demo/service/AcademicEventService.java
+++ src/main/java/com/example/demo/service/AcademicEventService.java
@@ -3,3 +3,7 @@ package com.example.demo.service;
 import jakarta.persistence.service;
 import com.example.demo.entity.AcademicEvent;
 
+@Service
+public interface AcademicCalenderService{
+    public AcademicCalender 
+}
