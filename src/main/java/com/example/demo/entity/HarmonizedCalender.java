1a2c6bcf5f8ff130de7007d6957c97009a6c7aae
bala-amypo <bala@amypo.in>
2025-12-18 06:51:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/HarmonizedCalender.java
+++ src/main/java/com/example/demo/entity/HarmonizedCalender.java
@@ -2,11 +2,21 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 
+
 @Entity
 public class HarmonizedCalender {
+
     private Long id;
     private String title;
     private String generatedBy;
     private LocalDateTime generatedAt;
-    
+    private LocalDate effectiveTo;
+    private LocalDate effectiveFrom;
+    private String eventsJson;
+
+    public void ValidateAndInitializer() {
+        this.effectiveTo = LocalDate.now();
+        this.effectiveFrom = LocalDate.now();
+        this.generatedAt = LocalDateTime.now();
+    }
 }
