9cfb4133b735fdbbffc24de54c9d782ca5118eae
bala-amypo <bala@amypo.in>
2025-12-18 09:52:33 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/ClashRecordRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.BranchProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BranchProfileRepository extends JpaRepository<BranchProfile, Long>{
+
+}
