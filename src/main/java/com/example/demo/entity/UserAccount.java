43386a240aca2e7214331c896e9dc141ce862bd8
bala-amypo <bala@amypo.in>
2025-12-18 08:21:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -16,6 +16,78 @@ public class UserAccount {
     private LocalDateTime createdAt;
 
     public void ValidateAndInitializer() {
-        this.
+        this.createdAt = LocalDateTime.now();
     }
+
+    public UserAccount(LocalDateTime createdAt, String department, String email, String fullName, Long id, String password, String role) {
+        this.createdAt = createdAt;
+        this.department = department;
+        this.email = email;
+        this.fullName = fullName;
+        this.id = id;
+        this.password = password;
+        this.role = role;
+    }
+
+    public void ValidateAndInitializer() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+
 }
