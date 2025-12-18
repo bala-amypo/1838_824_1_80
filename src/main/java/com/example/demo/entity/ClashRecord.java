991367bbefdfa1848762b246c7b2af863988e687
bala-amypo <bala@amypo.in>
2025-12-18 06:21:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/ClashRecord.java
+++ src/main/java/com/example/demo/entity/ClashRecord.java
@@ -1,3 +1,15 @@
 package com.example.demo.entity;
 
-import
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class ClashRecord {
+    
+    @Id
+    private Long id;
+    private Long eventAId;
+    private Long eventBId;
+    private String clashType;
+    
+}
