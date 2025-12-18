2594dbd37b97cb7c05f995b13cb1a622e2595534
bala-amypo <bala@amypo.in>
2025-12-18 06:26:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/ClashRecord.java
+++ src/main/java/com/example/demo/entity/ClashRecord.java
@@ -2,6 +2,9 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import java.lang.Boolean;
+import java.time.LocalDateTime;
+
 
 @Entity
 public class ClashRecord {
@@ -11,5 +14,14 @@ public class ClashRecord {
     private Long eventAId;
     private Long eventBId;
     private String clashType;
+    private String severity;
+    private String details;
+    private LocalDateTime detectedAt;
+    private Boolean resolved;
+
+    public void ValidateAndInitializer() {
+        this.detectedAt = LocalDateTime.now();
+
+    }
     
 }
