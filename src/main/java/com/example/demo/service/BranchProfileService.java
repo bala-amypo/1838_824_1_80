87d7ae8a32105530841ac956557d49351e0dfa7b
bala-amypo <bala@amypo.in>
2025-12-18 13:15:47 +0000

    User commit

--- src/main/java/com/example/demo/service/BranchProfileService.java
+++ src/main/java/com/example/demo/service/BranchProfileService.java
@@ -4,5 +4,8 @@ import com.example.demo.entity.BranchProfile;
 import jakarta.persistence.service;
 
 public interface BranchProfileService {
-    
+    public BranchProfile createBranch(BranchProfile branch);
+    public BranchProfile updateBranchStatus(Long id, boolean active);
+    public BranchProfile getAllBranches();
+    public 
 }
