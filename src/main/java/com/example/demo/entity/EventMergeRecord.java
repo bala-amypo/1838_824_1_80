890a5ed8f17714797432fb66c3d9f1ac1cca46bd
bala-amypo <bala@amypo.in>
2025-12-18 05:41:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/EventMergeRecord.java
+++ src/main/java/com/example/demo/entity/EventMergeRecord.java
@@ -29,5 +29,72 @@ public class EventMergeRecord {
     public void PrePersist() {
         this.createdAt = LocalDateTime.now();
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSourceEventIds() {
+        return sourceEventIds;
+    }
+
+    public void setSourceEventIds(String sourceEventIds) {
+        this.sourceEventIds = sourceEventIds;
+    }
+
+    public String getMergedTitle() {
+        return mergedTitle;
+    }
+
+    public void setMergedTitle(String mergedTitle) {
+        this.mergedTitle = mergedTitle;
+    }
+
+    public LocalDate getMergedStartDate() {
+        return mergedStartDate;
+    }
+
+    public void setMergedStartDate(LocalDate mergedStartDate) {
+        this.mergedStartDate = mergedStartDate;
+    }
+
+    public LocalDate getMergedEndDate() {
+        return mergedEndDate;
+    }
+
+    public void setMergedEndDate(LocalDate mergedEndDate) {
+        this.mergedEndDate = mergedEndDate;
+    }
+
+    public String getMergeReason() {
+        return mergeReason;
+    }
+
+    public void setMergeReason(String mergeReason) {
+        this.mergeReason = mergeReason;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public EventMergeRecord(LocalDateTime createdAt, Long id, String mergeReason, LocalDate mergedEndDate, LocalDate mergedStartDate, String mergedTitle, String sourceEventIds) {
+        this.createdAt = createdAt;
+        this.id = id;
+        this.mergeReason = mergeReason;
+        this.mergedEndDate = mergedEndDate;
+        this.mergedStartDate = mergedStartDate;
+        this.mergedTitle = mergedTitle;
+        this.sourceEventIds = sourceEventIds;
+    }
+    public EventMergeRecord() {
     
+}
 }
