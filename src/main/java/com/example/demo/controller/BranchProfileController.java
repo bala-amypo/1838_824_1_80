6040a51d52932a1c8677b981e2e6da9eea47924d
bala-amypo <bala@amypo.in>
2025-12-19 09:43:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/BranchProfileController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.autowired;
+import org.springframework.beans.factory.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.PutMapping;
+import com.example.demo.entity.BranchProfile;
+import com.example.demo.services.BranchProfileService;
+
+@RestController
+@RequestMapping("/api/academicevents")
+public class AcademicEventController {
+
+    @autowired
+    private AcademicEventService academicEventService;
+
+    @PostMapping
+    public AcademicEvent createEvent(@RequestBody AcademicEvent event) {
+        return academicEventService.createEvent(event);
+    }
+    @GetMapping
+    public List<AcademicEvent> getAllEvents() {
+        return academicEventService.getAllEvents();
+    }
+    @GetMapping("/{id}")
+    public AcademicEvent getEventById(@PathVariable Long id) {
+        return academicEventService.getEventById(id);
+    }
+    @GetMapping("/branch/{branchId}")
+    public List<academicEvent> getEventsByBranch(@PathVariable Long branchId){
+        return academicEventService.getEventsByBranch(branchId);
+    }
+    @PutMapping("/{id}")
+    public AcademicEvent updateEvent(@PathVariable Longid, @RequestBody AcademicEvent event){
+        return academicEventService.updateEvent(id,event);
+    }
+
+}
