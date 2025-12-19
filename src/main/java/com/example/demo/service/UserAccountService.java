c0c46975883e437e6c5cada5c21d9b34db0b604b
bala-amypo <bala@amypo.in>
2025-12-19 03:54:49 +0000

    User commit

--- src/main/java/com/example/demo/service/UserAccountService.java
+++ src/main/java/com/example/demo/service/UserAccountService.java
@@ -1,1 +1,11 @@
 package com.example.demo.service;
+
+package com.example.demo.service;
+
+import com.example.demo.entity.HarmonizedCalendar; 
+import jakarta.persistence.Service;
+
+@Service
+public interface UserAccountService {
+    public 
+}
