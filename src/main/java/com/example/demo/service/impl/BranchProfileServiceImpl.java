5be29da04237b9931aa76b9a2e4dd4f3c5bfd811
bala-amypo <bala@amypo.in>
2025-12-19 08:01:29 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BranchProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BranchProfileServiceImpl.java
@@ -3,3 +3,4 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.BranchProfile;
 import com.example.demo.repository.BranchProfileRepository;
 import com.example.demo.service.BranchProfileService;
+import org.springframework.stereotype.Service;
