407bf7bb8916716ea4b79ee0047115df0432024c
bala-amypo <bala@amypo.in>
2025-12-19 05:32:33 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AcademicEventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AcademicEventServiceImpl.java
@@ -7,4 +7,9 @@ import org.springframework.sterotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
-public class AcademicEventServiceImpl implements AcademicEventService
+public class AcademicEventServiceImpl implements AcademicEventService {
+
+    @Autowired
+    AcademicEventRepository aer;
+
+}
