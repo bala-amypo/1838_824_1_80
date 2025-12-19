162b2fb381ef360ef8f0f42768421d31bf353eec
bala-amypo <bala@amypo.in>
2025-12-19 08:40:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/AcademicEventController.java
+++ src/main/java/com/example/demo/controller/AcademicEventController.java
@@ -1,4 +1,9 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import org.springframework.beans.
+import org.springframework.beans.factory.annotation.autowired;
+import com.example.demo.entity.AcademicEvent;
+import com.example.demo.services.AcademicEventService;
+
+@RestController
+@RequestMapping()
