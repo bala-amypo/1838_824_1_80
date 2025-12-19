4edf791068bf225b82db1fce6270115608b50239
bala-amypo <bala@amypo.in>
2025-12-19 09:40:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/BranchProfileRepository.java
+++ src/main/java/com/example/demo/repository/BranchProfileRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.BranchProfile;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BranchProfileRepository extends JpaRepository<BranchProfile, Long>{
-
+    Optional<BranchProfile> findByBranchCode(String branchCode);
 }
--- src/main/java/com/example/demo/service/impl/BranchProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BranchProfileServiceImpl.java
@@ -1,11 +1,51 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.BranchProfile;
 import com.example.demo.repository.BranchProfileRepository;
 import com.example.demo.service.BranchProfileService;
-import org.springframework.stereotype.Service;
 
 @Service
 public class BranchProfileServiceImpl implements BranchProfileService {
-    
+
+    @Autowired
+    private BranchProfileRepository branchProfileRepository;
+
+    @Override
+    public BranchProfile createBranch(BranchProfile branch) {
+        return branchProfileRepository.save(branch);
+    }
+
+    @Override
+    public BranchProfile updateBranchStatus(Long id, boolean active) {
+
+        BranchProfile branch = branchProfileRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Branch not found with id: " + id));
+
+        branch.setActive(active);
+
+        return branchProfileRepository.save(branch);
+    }
+
+    @Override
+    public List<BranchProfile> getAllBranches() {
+        return branchProfileRepository.findAll();
+    }
+
+    @Override
+    public BranchProfile getBranchById(Long id) {
+        return branchProfileRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Branch not found with id: " + id));
+    }
+
+    @Override
+    public BranchProfile findByBranchCode(String branchCode) {
+        return branchProfileRepository.findByBranchCode(branchCode)
+                .orElseThrow(() -> new RuntimeException(
+                        "Branch not found with code: " + branchCode));
+    }
 }
