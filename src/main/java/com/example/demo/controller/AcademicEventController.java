399db70a807621dbafee03f03b58152a3d7c6d41
bala-amypo <bala@amypo.in>
2025-12-19 15:06:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/AcademicEventController.java
+++ src/main/java/com/example/demo/controller/AcademicEventController.java
@@ -5,8 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entity.AcademicEvent;
-import com.example.demo.services.AcademicEventService;
+import com.example.demo.service.AcademicEventService;
 
 @RestController
 @RequestMapping("/api/academic-events")
--- src/main/java/com/example/demo/controller/BranchProfileController.java
+++ src/main/java/com/example/demo/controller/BranchProfileController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entity.BranchProfile;
 import com.example.demo.services.BranchProfileService;
 
