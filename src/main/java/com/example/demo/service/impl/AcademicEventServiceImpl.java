ac09aa532e78843331319ca0304a58a573250dc8
bala-amypo <bala@amypo.in>
2025-12-19 06:09:08 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AcademicEventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AcademicEventServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.AcademicEvent;
+import java.util.List;
 import com.example.demo.repository.AcademicEventRepository;
 import com.example.demo.service.AcademicEventService;
 import org.springframework.sterotype.Service;
@@ -10,7 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class AcademicEventServiceImpl implements AcademicEventService {
 
     @Autowired
-    AcademicEventRepository aer;
+    private AcademicEventRepository aer;
+    @Override
     public AcademicEvent createAcademicEvent(AcademicEvent Event){
         return aer.save(Event)
     }
