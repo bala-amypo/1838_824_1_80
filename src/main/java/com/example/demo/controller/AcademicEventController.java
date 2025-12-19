39405ed7047f29851f0824ee9a5a0b80fa891c1b
bala-amypo <bala@amypo.in>
2025-12-19 08:46:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/AcademicEventController.java
+++ src/main/java/com/example/demo/controller/AcademicEventController.java
@@ -8,5 +8,14 @@ import com.example.demo.services.AcademicEventService;
 @RestController
 @RequestMapping("/api/academicevents")
 public class AcademicEventController {
+
+    @autowired
+    private AcademicEventService academicEventService;
+
+    @PostMapping
+    public AcademicEvent createEvent(@RequestBody AcademicEvent event) {
+        return academicEventService.createEvent(event);
+
+    }
     
 }
