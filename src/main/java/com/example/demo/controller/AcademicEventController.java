41b46715c52f1188fc0453d26db374ddcb72abf2
bala-amypo <bala@amypo.in>
2025-12-19 09:58:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/AcademicEventController.java
+++ src/main/java/com/example/demo/controller/AcademicEventController.java
@@ -1,39 +1,34 @@
-package com.example.demo.controller;
-
-import java.util.List;
-import org.springframework.beans.factory.annotation.autowired;
-import org.springframework.beans.factory.annotation.GetMapping;
-import org.springframework.beans.factory.annotation.PostMapping;
-import org.springframework.beans.factory.annotation.PutMapping;
-import com.example.demo.entity.AcademicEvent;
-import com.example.demo.services.AcademicEventService;
-
 @RestController
-@RequestMapping("/api/events")
+@RequestMapping("/api/academic-events")
 public class AcademicEventController {
 
-    @autowired
+    @Autowired
     private AcademicEventService academicEventService;
 
     @PostMapping
     public AcademicEvent createEvent(@RequestBody AcademicEvent event) {
         return academicEventService.createEvent(event);
     }
+
     @GetMapping
     public List<AcademicEvent> getAllEvents() {
         return academicEventService.getAllEvents();
     }
+
     @GetMapping("/{id}")
     public AcademicEvent getEventById(@PathVariable Long id) {
         return academicEventService.getEventById(id);
     }
+
     @GetMapping("/branch/{branchId}")
-    public List<academicEvent> getEventsByBranch(@PathVariable Long branchId){
+    public List<AcademicEvent> getEventsByBranch(@PathVariable Long branchId) {
         return academicEventService.getEventsByBranch(branchId);
     }
-    @PutMapping("/{id}")
-    public AcademicEvent updateEvent(@PathVariable Longid, @RequestBody AcademicEvent event){
-        return academicEventService.updateEvent(id,event);
-    }
 
+    @PutMapping("/{id}")
+    public AcademicEvent updateEvent(
+            @PathVariable Long id,
+            @RequestBody AcademicEvent event) {
+        return academicEventService.updateEvent(id, event);
+    }
 }
