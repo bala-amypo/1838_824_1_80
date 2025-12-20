e39d4c4567d248dc63128129ecf5da5f1d9b94c4
bala-amypo <bala@amypo.in>
2025-12-20 05:27:16 +0000

    User commit

--- src/main/java/com/example/demo/repository/HarmonizedCalendarRepository.java
+++ src/main/java/com/example/demo/repository/HarmonizedCalendarRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.HarmonizedCalendar;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-import java.time.LocalDate
+import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.UserAccount;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
