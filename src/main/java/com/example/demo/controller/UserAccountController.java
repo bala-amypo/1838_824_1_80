b40b5faeb59b5ce13f453d2a0f47ce9c745a9a32
bala-amypo <bala@amypo.in>
2025-12-22 03:47:14 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserAccountController.java
+++ src/main/java/com/example/demo/controller/UserAccountController.java
@@ -26,7 +26,7 @@ public class UserAccountController {
     // POST /auth/login - Login and return JWT token
     @PostMapping("/login")
     public Map<String, String> login(@RequestBody UserAccount user) {
-        String token = service.login(user.getFullname(), user.getPassword());
+        String token = service.login(user.getFullName(), user.getPassword());
         return Map.of("token", token);
     }
 
