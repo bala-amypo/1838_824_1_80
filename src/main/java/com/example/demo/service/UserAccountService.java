c9154dfcefc66d3e5edbe207f7871879bc41d5ab
bala-amypo <bala@amypo.in>
2025-12-27 05:29:39 +0000

    User commit

--- src/main/java/com/example/demo/service/UserAccountService.java
+++ src/main/java/com/example/demo/service/UserAccountService.java
@@ -1,14 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.UserAccount;
-
 import java.util.List;
 
 public interface UserAccountService {
 
-    UserAccount register(UserAccount user);
-
-    String login(String fullName, String password);
+    UserAccount createUser(UserAccount userAccount);
 
     List<UserAccount> getAllUsers();
 
--- src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
@@ -3,49 +3,32 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.UserAccount;
 import com.example.demo.repository.UserAccountRepository;
 import com.example.demo.service.UserAccountService;
-import org.springframework.stereotype.Service;
 
+import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class UserAccountServiceImpl
-        implements UserAccountService {
+public class UserAccountServiceImpl implements UserAccountService {
 
-    private final UserAccountRepository repository;
+    private final UserAccountRepository userAccountRepository;
 
-    public UserAccountServiceImpl(UserAccountRepository repository) {
-        this.repository = repository;
+    public UserAccountServiceImpl(UserAccountRepository userAccountRepository) {
+        this.userAccountRepository = userAccountRepository;
     }
 
     @Override
-    public UserAccount register(UserAccount user) {
-        user.setRole("USER");
-        return repository.save(user);
-    }
-
-    @Override
-    public String login(String fullName, String password) {
-
-        UserAccount user = repository.findByFullName(fullName)
-                .orElseThrow(() -> new RuntimeException("Invalid full name"));
-
-        if (!user.getPassword().equals(password)) {
-            throw new RuntimeException("Invalid password");
-        }
-
-        return "LOGIN_SUCCESS";
+    public UserAccount createUser(UserAccount userAccount) {
+        return userAccountRepository.save(userAccount);
     }
 
     @Override
     public List<UserAccount> getAllUsers() {
-        return repository.findAll();
+        return userAccountRepository.findAll();
     }
 
-    
     @Override
-public UserAccount getUser(long id) {
-    return repository.findById(id)
-            .orElseThrow(() -> new RuntimeException("User not found"));
-}
-
+    public UserAccount getUserById(Long id) {
+        return userAccountRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
 }
